a17d7f0b97fc3b856ab359413b995ee9ba94777d
AlexKnauth <alexander@knauth.org>
2016-07-27 20:23:33 -0400

    add general show-adt form

---
+++ gnal-lambda/show-adt.rkt
@@ -0,0 +1,76 @@
+#lang racket/base
+
+(provide show-adt)
+
+(require "lc/io.rkt"
+         (for-syntax racket/base
+                     syntax/parse
+                     syntax/stx
+                     "io.rkt"
+                     "adt/private/conversions.rkt"
+                     ))
+
+(define space-byte
+  (byte (0-bit) (0-bit) (0-bit) (0-bit) (0-bit) (1-bit) (0-bit) (0-bit)))
+
+(define open-paren-byte
+  (byte (0-bit) (0-bit) (0-bit) (1-bit) (0-bit) (1-bit) (0-bit) (0-bit)))
+(define close-paren-byte
+  (byte (1-bit) (0-bit) (0-bit) (1-bit) (0-bit) (1-bit) (0-bit) (0-bit)))
+
+(define space (byte-string1 space-byte))
+(define open-paren (byte-string1 open-paren-byte))
+(define close-paren (byte-string1 close-paren-byte))
+
+(define-syntax byte-string-append*
+  (lambda (stx)
+    (syntax-parse stx
+      [(bytes-string-append* a:expr) #'a]
+      [(bytes-string-append* a:expr b:expr ...+)
+       #'(byte-string-append a (byte-string-append* b ...))])))
+
+(define add-space
+  (λ (bstr)
+    (byte-string-append space bstr)))
+
+(define-syntax show-adt
+  (lambda (stx)
+    (syntax-parse stx
+      [(show-adt adt-type:id
+         (variant:id show-element:expr ...)
+         ...)
+       #:with [variant-bstr:id ...] (generate-temporaries #'[variant ...])
+       #:with [[field:id ...] ...] (stx-map generate-temporaries #'[[show-element ...] ...])
+       #'(let ([variant-bstr (byte-string-literal/id variant)] ...)
+           (λ (v)
+             (match-adt adt-type v
+               [(variant field ...)
+                (byte-string-append*
+                 open-paren
+                 variant-bstr
+                 (add-space (show-element field))
+                 ...
+                 close-paren)]
+               ...)))])))
+
+(define-syntax byte-string-literal/id
+  (lambda (stx)
+    (syntax-parse stx
+      [(byte-string-literal/id id:id)
+       (define bs (bytes->list (string->bytes/utf-8 (symbol->string (syntax-e #'id)))))
+       (define lcbs (map rkt->byte bs))
+       #`(byte-string-append*
+          #,@(for/list ([lcbyte (in-list lcbs)])
+               #`(byte-string1
+                  (byte
+                   #,@
+                   (lcbyte (λ (b0 b1 b2 b3 b4 b5 b6 b7)
+                             (list (b0 (λ () #'(0-bit)) (λ () #'(1-bit)))
+                                   (b1 (λ () #'(0-bit)) (λ () #'(1-bit)))
+                                   (b2 (λ () #'(0-bit)) (λ () #'(1-bit)))
+                                   (b3 (λ () #'(0-bit)) (λ () #'(1-bit)))
+                                   (b4 (λ () #'(0-bit)) (λ () #'(1-bit)))
+                                   (b5 (λ () #'(0-bit)) (λ () #'(1-bit)))
+                                   (b6 (λ () #'(0-bit)) (λ () #'(1-bit)))
+                                   (b7 (λ () #'(0-bit)) (λ () #'(1-bit))))))))))])))
+
--- λ/tests/print-list.rkt
+++ λ/tests/print-list.rkt
@@ -1,88 +1,11 @@
 #lang gnal λ/io
 
-(define space-byte
-  (byte (0-bit) (0-bit) (0-bit) (0-bit) (0-bit) (1-bit) (0-bit) (0-bit)))
-
-(define open-paren-byte
-  (byte (0-bit) (0-bit) (0-bit) (1-bit) (0-bit) (1-bit) (0-bit) (0-bit)))
-(define close-paren-byte
-  (byte (1-bit) (0-bit) (0-bit) (1-bit) (0-bit) (1-bit) (0-bit) (0-bit)))
+(require "../../gnal-lambda/show-adt.rkt")
 
 (define a-byte
   (byte (1-bit) (0-bit) (0-bit) (0-bit) (0-bit) (1-bit) (1-bit) (0-bit)))
 (define b-byte
   (byte (0-bit) (1-bit) (0-bit) (0-bit) (0-bit) (1-bit) (1-bit) (0-bit)))
-(define c-byte
-  (byte (1-bit) (1-bit) (0-bit) (0-bit) (0-bit) (1-bit) (1-bit) (0-bit)))
-;d
-(define e-byte
-  (byte (1-bit) (0-bit) (1-bit) (0-bit) (0-bit) (1-bit) (1-bit) (0-bit)))
-;fghijkl
-(define m-byte
-  (byte (1-bit) (0-bit) (1-bit) (1-bit) (0-bit) (1-bit) (1-bit) (0-bit)))
-(define n-byte
-  (byte (0-bit) (1-bit) (1-bit) (1-bit) (0-bit) (1-bit) (1-bit) (0-bit)))
-(define o-byte
-  (byte (1-bit) (1-bit) (1-bit) (1-bit) (0-bit) (1-bit) (1-bit) (0-bit)))
-(define p-byte
-  (byte (0-bit) (0-bit) (0-bit) (0-bit) (1-bit) (1-bit) (1-bit) (0-bit)))
-;qr
-(define s-byte
-  (byte (1-bit) (1-bit) (0-bit) (0-bit) (1-bit) (1-bit) (1-bit) (0-bit)))
-(define t-byte
-  (byte (0-bit) (0-bit) (1-bit) (0-bit) (1-bit) (1-bit) (1-bit) (0-bit)))
-;uvwx
-(define y-byte
-  (byte (1-bit) (0-bit) (0-bit) (1-bit) (1-bit) (1-bit) (1-bit) (0-bit)))
-;z
-
-;; bstr:empty : Byte-String
-(define bstr:empty
-  (byte-string-append
-   (byte-string1 e-byte)
-   (byte-string-append
-    (byte-string1 m-byte)
-    (byte-string-append
-     (byte-string1 p-byte)
-     (byte-string-append
-      (byte-string1 t-byte)
-      (byte-string1 y-byte))))))
-
-;; bstr:cons : Byte-String
-(define bstr:cons
-  (byte-string-append
-   (byte-string1 c-byte)
-   (byte-string-append
-    (byte-string1 o-byte)
-    (byte-string-append
-     (byte-string1 n-byte)
-     (byte-string1 s-byte)))))
-
-;; make-empty-bstr : -> Byte-String
-(define make-empty-bstr
-  (λ ()
-    (byte-string-append
-     (byte-string1 open-paren-byte)
-     (byte-string-append
-      bstr:empty
-      (byte-string1 close-paren-byte)))))
-
-;; make-cons-bstr : Byte-String Byte-String -> Byte-String
-(define make-cons-bstr
-  (λ (a b)
-    (byte-string-append
-     (byte-string1 open-paren-byte)
-     (byte-string-append
-      bstr:cons
-      (byte-string-append
-       (byte-string1 space-byte)
-       (byte-string-append
-        a
-        (byte-string-append
-         (byte-string1 space-byte)
-         (byte-string-append
-          b
-          (byte-string1 close-paren-byte)))))))))
 
 ;; A (Listof A) is one of:
 ;;  - (empty)
@@ -93,13 +16,9 @@
 
 (define show-list
   (λ (show-element)
-    (λ (lst)
-      (match-adt List lst
-        [(empty) (make-empty-bstr)]
-        [(cons first rest)
-         (make-cons-bstr
-          (show-element first)
-          ((show-list show-element) rest))]))))
+    (show-adt List
+      (empty)
+      (cons show-element (show-list show-element)))))
 
 (define show-bit
   (λ (b)
